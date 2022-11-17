96c10067a7c9219db82c263e5c996abad735d28e
RenatoLemes <renato.lemmes@hotmail.com>
2022-11-17 17:43:46 +0000

    Updating code

--- run.py
+++ run.py
@@ -13,14 +13,14 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('the_fruit_shop')
 
 FRUIT_LIST = {
-    Banana Box 10kg
-    Apple Box 10kg
-    Pear Box 10kg
-    Pineapple 12 units
-    Papaya Box 10kg
-    Avocado Box 10kg
-    Orange Box 10kg
-    Lemon box 5kg
+    Banana Box 10kg - € 14.99
+    Apple Box 10kg - € 14.99
+    Pear Box 10kg - € 14.99
+    Pineapple 12 units - € 11.99
+    Papaya Box 10 units - € 16.99
+    Avocado Box 10kg - € 21.99
+    Orange Box 10kg - € 14.99
+    Lemon box 5kg - € 8.99
 }
 
 def get_order():
@@ -28,4 +28,4 @@ def get_order():
     Get order input from the costumer
     """
 
-    print("Please, enter your product order")
+    print("Please, enter your order")
